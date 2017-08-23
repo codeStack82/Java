712d680760321b67266ae2821870f572d6221257
“[Tyler <[codestack82@gmail.com]>
2017-08-23 12:13:44 -0500

    commit all

---
+++ Adv Java/temp/Hunt_W1_BaseConversion.java
@@ -0,0 +1,38 @@
+/*
+ * Tyler Hunt
+ * Advanced Java
+ * OCCC Fall 2017
+ * Class Example
+ * Due: 08/23/17
+*/
+
+/* Week 1 - Outline
+    - Check inputs 
+        - cmd line inputs?
+        - User enter input?
+
+    - Intro message
+        - if cmd
+            - get inputs
+
+        - if user input 
+            - get inputs
+                - prompt for string to conv, base, output
+    
+    - Validate inputs
+        - Check if string is valid against initial base 
+            - If valid 
+                - Convert to base 10 (Use BigInt)
+                - Convert to target base (use BigInt)
+                - Return value as String from BigInt 
+            - Else return error message
+                - Wrap in do/while to get another set of inputs 
+*/
+
+public class Hunt_W1_BaseConversion{
+    public static void main(String [] args){
+
+        
+        
+    }//eoMain
+}
